8bc0e2b2551ea1c941f03daf65c1892f0c567647
anaturcu <turcu.anamaria98@gmail.com>
2020-12-08 08:37:58 +0200

    insert traffic data

--- trafficMS/src/main/java/DatabaseConnection/DatabaseConnection.java
+++ trafficMS/src/main/java/DatabaseConnection/DatabaseConnection.java
@@ -1,8 +1,6 @@
 package DatabaseConnection;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DatabaseConnection {
     public static DatabaseConnection instance = null;
@@ -25,6 +23,33 @@ public class DatabaseConnection {
         return instance;
     }
 
+    public int getTrafficId() {
+        int id = 0;
+        try {
+            PreparedStatement statement = conn.prepareStatement("select count(*) from traffic");
+            ResultSet resultSet = statement.executeQuery();
+            resultSet.next();
+            id = resultSet.getInt(1);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return id + 1;
+    }
+
+    public Boolean addTraffic(int id_traffic_light, int nrCarsDetected, String image) {
+        try {
+            PreparedStatement statement = conn.prepareStatement("insert into traffic(id, id_traffic_light, nr_cars, image) values(?, ?, ?, ?)");
+            statement.setInt(1, getTrafficId());
+            statement.setInt(2, id_traffic_light );
+            statement.setInt(3, nrCarsDetected);
+            statement.setString(4, image);
+            statement.executeUpdate();
+        } catch (Exception throwables) {
+            throwables.printStackTrace();
+            return false;
+        }
+        return true;
+    }
 
 
 }
--- trafficMS/src/main/java/Handlers/AddTrafficHandler.java
+++ trafficMS/src/main/java/Handlers/AddTrafficHandler.java
@@ -1,6 +1,7 @@
 package Handlers;
 
 import DTOs.Commands.AddTrafficCommand;
+import DatabaseConnection.DatabaseConnection;
 import com.google.gson.Gson;
 
 public class AddTrafficHandler implements Handler{
@@ -11,15 +12,19 @@ public class AddTrafficHandler implements Handler{
     }
 
     AddTrafficCommand command;
+    DatabaseConnection databaseConnection;
 
     public AddTrafficHandler(AddTrafficCommand command) {
         this.command= command;
+        databaseConnection = DatabaseConnection.getInstance();
     }
 
     @Override
     public String handle() {
         Response response = new Response();
-
+        response.success = databaseConnection.addTraffic(command.getIdTrafficLight(),
+                command.getNrCars(),
+                command.getImage());
         Gson gson = new Gson();
         return gson.toJson(response);
     }
